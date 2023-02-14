e71e545db891f1d77136c7dc0b03430bbc40d58e
cola639 <337507950@qq.com>
2023-02-14 16:56:40 +0800

    first commit

--- src/components/button.component.tsx
+++ src/components/button.component.tsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React from "react"
 import {
   Button as MuiButton,
   ButtonProps as MuiButtonProps,
-} from "@mui/material";
+} from "@mui/material"
 
 // Only include variant, size, color from MuiButtonProps
-type ButtonBaseProps = Pick<MuiButtonProps, "variant" | "size" | "color">;
+type ButtonBaseProps = Pick<MuiButtonProps, "variant" | "size" | "color">
 
 // Include everything from MuiButtonProps except disableRipple
 // type ButtonBaseProps = Omit<MuiButtonProps, "disableRipple">;
 
 export interface ButtonProps extends ButtonBaseProps {
-  label: string;
+  label: string
 }
 
 export const Button = ({ label, ...rest }: ButtonProps) => (
-  <MuiButton {...rest}>{label}</MuiButton>
-);
+  <MuiButton {...rest}>{label}自定义组件</MuiButton>
+)
 
 Button.defaultProps = {
   variant: "contained",
   size: "medium",
   color: "primary",
-};
+}
